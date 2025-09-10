ff9d249d0b58498b0a1717848bdaca06cfc6b281
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2025-09-10 17:17:18 +0200

    feat: switch to toolkit-lib

--- src/cli.ts
+++ src/cli.ts
@@ -1,56 +1,40 @@
 #!/usr/bin/env node
-import {
-    AwsCdkCli,
-    type ICloudAssemblyDirectoryProducer,
-    RequireApproval,
-} from "@aws-cdk/cli-lib-alpha";
+import { Toolkit } from "@aws-cdk/toolkit-lib";
 import { CloudFormation } from "@aws-sdk/client-cloudformation";
-import { App, type AppProps } from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { Command } from "commander";
 import { ProviderStack } from "./provider-stack.js";
 import { RoleStack } from "./role-stack.js";
-
-class Producer implements ICloudAssemblyDirectoryProducer {
-    public constructor(
-        private namespace: string,
-        private repositoryName: string,
-        private repositoryUuid: string,
-    ) {}
-
-    public async produce(context: AppProps["context"]): Promise<string> {
-        const app = new App({ context });
-
-        const baseProps = {
-            repositoryUuid: this.repositoryUuid,
-            bitbucketDomain:
-                "api.bitbucket.org/2.0/workspaces/soliantconsulting/pipelines-config/identity/oidc",
-            bitbucketAudience:
-                "ari:cloud:bitbucket::workspace/edf547a3-8e06-4217-abd8-7b9139a21e2c",
-        };
-
-        const providerStack = new ProviderStack(app, `${this.namespace}-provider`, baseProps);
-
-        new RoleStack(app, `${this.namespace}-role-${this.repositoryName}`, {
-            ...baseProps,
-            providerStack,
-        });
-
-        return app.synth().directory;
-    }
-}
 
 const program = new Command();
 
 program
     .command("deploy <namespace> <repository-name> <repository-uuid>")
     .action(async (namespace: string, repositoryName: string, repositoryUuid: string) => {
-        const cli = AwsCdkCli.fromCloudAssemblyDirectoryProducer(
-            new Producer(namespace, repositoryName, repositoryUuid),
-        );
+        const toolkit = new Toolkit();
 
-        await cli.deploy({
-            requireApproval: RequireApproval.NEVER,
-        });
+        await toolkit.deploy(
+            await toolkit.fromAssemblyBuilder(async () => {
+                const app = new App();
+
+                const baseProps = {
+                    repositoryUuid: repositoryUuid,
+                    bitbucketDomain:
+                        "api.bitbucket.org/2.0/workspaces/soliantconsulting/pipelines-config/identity/oidc",
+                    bitbucketAudience:
+                        "ari:cloud:bitbucket::workspace/edf547a3-8e06-4217-abd8-7b9139a21e2c",
+                };
+
+                const providerStack = new ProviderStack(app, `${namespace}-provider`, baseProps);
+
+                new RoleStack(app, `${namespace}-role-${repositoryName}`, {
+                    ...baseProps,
+                    providerStack,
+                });
+
+                return app.synth();
+            }),
+        );
     });
 
 program
